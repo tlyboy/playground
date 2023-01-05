b9bb37ec7c607ff5f53f1a256aac4f9de0a5b51c
Guany <1084072690@qq.com>
2023-01-05 15:31:10 +0800

    chore: update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import 'normalize.css'
-import './assets/base.css'
-import './assets/main.css'
+import './assets/styles/base.css'
+import './assets/styles/main.css'
 import monaco from './utils/monaco'
 
 monaco.editor.create(document.getElementById('app'), {
