f1a2e42f8677e93763d88a7f72431cb7a9e44ecf
Guany <1084072690@qq.com>
2023-01-07 12:51:20 +0800

    feat: use vitesse theme

--- src/main.js
+++ src/main.js
@@ -2,8 +2,23 @@ import 'normalize.css'
 import './assets/styles/base.css'
 import './assets/styles/main.css'
 import monaco from './utils/monaco'
+import vitesseDark from 'theme-vitesse/themes/vitesse-dark.json'
+
+monaco.editor.defineTheme('vitesse-dark', vitesseDark)
 
 monaco.editor.create(document.getElementById('app'), {
   value: '',
-  language: 'javascript'
+  language: 'javascript',
+  theme: 'vitesse-dark',
+  cursorSmoothCaretAnimation: true,
+  detectIndentation: false,
+  fontFamily:
+    "ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+  fontSize: 16,
+  smoothScrolling: true,
+  tabSize: 2,
+  unicodeHighlight: {
+    ambiguousCharacters: false,
+    invisibleCharacters: false
+  }
 })
