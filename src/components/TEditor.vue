d7798932f292e29ef04fb54b3989a934f13a3ec9
Guany <1084072690@qq.com>
2023-12-02 15:50:00 +0800

    feat: add prettier

--- src/components/TEditor.vue
+++ src/components/TEditor.vue
@@ -2,9 +2,39 @@
 import * as monaco from 'monaco-editor'
 import vitesseDark from 'theme-vitesse/themes/vitesse-dark.json'
 import vitesseLight from 'theme-vitesse/themes/vitesse-light.json'
+import * as prettier from 'prettier/standalone'
+import prettierPluginBabel from 'prettier/plugins/babel'
+import prettierPluginEstree from 'prettier/plugins/estree'
+import prettierPluginHtml from 'prettier/plugins/html'
 
 monaco.editor.defineTheme('vitesse-dark', vitesseDark as any)
 monaco.editor.defineTheme('vitesse-light', vitesseLight as any)
+
+monaco.languages.registerDocumentFormattingEditProvider('javascript', {
+  async provideDocumentFormattingEdits(model) {
+    const text = await prettier.format(model.getValue(), {
+      parser: 'babel',
+      plugins: [prettierPluginBabel, prettierPluginEstree, prettierPluginHtml],
+      semi: false,
+      singleQuote: true,
+    })
+
+    return [
+      {
+        range: model.getFullModelRange(),
+        text,
+      },
+    ]
+  },
+})
+
+const editorRef = ref()
+
+async function formatCode() {
+  const editor = editorRef.value.$editor as monaco.editor.IStandaloneCodeEditor
+
+  editor.getAction('editor.action.formatDocument')?.run()
+}
 
 const { modelValue } = defineModels<{
   modelValue: string
@@ -13,6 +43,7 @@ const { modelValue } = defineModels<{
 
 <template>
   <TMonacoEditor
+    ref="editorRef"
     v-model="modelValue"
     v-bind="$attrs"
     :options="{
@@ -38,5 +69,6 @@ const { modelValue } = defineModels<{
         invisibleCharacters: false,
       },
     }"
+    @keydown.ctrl.s.prevent="formatCode"
   />
 </template>
