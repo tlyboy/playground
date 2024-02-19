83e3f43283638c0b23bd02389cfd5b13cfd2ecda
Guany <1084072690@qq.com>
2024-02-19 10:09:49 +0800

    chore: update

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
+// @ts-check
 import antfu from '@antfu/eslint-config'
-import unocss from '@unocss/eslint-plugin'
 
 export default antfu(
+  {
+    unocss: true,
+    formatters: true,
+  },
   {
     rules: {
       'no-console': 'off',
     },
   },
-  unocss.configs.flat,
 )
